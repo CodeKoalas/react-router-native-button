efb681813ff9bb3800d6976774ca7163e494ab05
Donnie West <Igneo676@gmail.com>
2017-03-17 16:47:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+# React Router Native Button
+
+```
+import RouterButton from 'react-router-native-button';
+
+<RouterButton
+  to="/settings"
+  title="Settings"
+  color="#841584"
+  accessibilityLabel="Click to navigate to settings"
+/>
+```
+
+```
+<RouterButton
+  to={{
+    pathname: '/courses',
+    search: '?sort=name',
+    hash: '#the-hash',
+    state: { fromDashboard: true }
+  title="Courses"
+  color="#841584"
+  accessibilityLabel="Click to navigate to settings"
+/>
+```
+
+## Install
+
+```
+npm install -g react-router-native-button
+```
+
+## Props
+
+```
+accessibilityLabel?: ?string 
+
+Text to display for blindness accessibility features
+```
+
+```
+color?: ?string 
+
+Color of the text (iOS), or background color of the button (Android)
+```
+
+```
+disabled?: ?boolean 
+
+If true, disable all interactions for this component.
+```
+
+```
+onPress: () => any 
+
+Handler to be called when the user taps the button
+```
+
+```
+testID?: ?string 
+
+Used to locate this view in end-to-end tests.
+```
+
+```
+title: string 
+
+Text to display inside the button
+```
+
+```
+to: ?string
+
+The pathname or location to link to.
+```
+
+```
+to: ?object
+
+The location to link to.
+```
+
+```
+replace: ?boolean
+
+When true, clicking the button will replace the current entry in the history stack instead of adding a new one.
+```
+
+## Credits
+
+[React Native](https://github.com/facebook/react-native) for the Button code
+[React Router](https://github.com/ReactTraining/react-router) for the Link code that's used here
---
+++ index.js
@@ -0,0 +1,167 @@
+import React, { PropTypes } from 'react';
+import {
+  ColorPropType,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { Link } from 'react-router-native';
+
+let defaultBlue = '#2196F3';
+if (Platform.OS === 'ios') {
+  defaultBlue = '#0C42FD';
+}
+
+const styles = StyleSheet.create({
+  button: Platform.select({
+    ios: {},
+    android: {
+      elevation: 4,
+      backgroundColor: defaultBlue,
+      borderRadius: 2,
+    },
+  }),
+  text: Platform.select({
+    ios: {
+      color: defaultBlue,
+      textAlign: 'center',
+      padding: 8,
+      fontSize: 18,
+    },
+    android: {
+      textAlign: 'center',
+      color: 'white',
+      padding: 8,
+      fontWeight: '500',
+    },
+  }),
+  buttonDisabled: Platform.select({
+    ios: {},
+    android: {
+      elevation: 0,
+      backgroundColor: '#dfdfdf',
+    },
+  }),
+  textDisabled: Platform.select({
+    ios: {
+      color: '#cdcdcd',
+    },
+    android: {
+      color: '#a1a1a1',
+    },
+  }),
+});
+
+const buttonStyles = [styles.button];
+const textStyles = [styles.text];
+const Touchable = Platform.OS === 'android'
+  ? TouchableNativeFeedback
+  : TouchableOpacity;
+
+function RouterButton(
+  {
+    accessibilityLabel,
+    color,
+    title,
+    disabled,
+    testID,
+    replace,
+    to,
+  },
+) {
+  if (color && Platform.OS === 'ios') {
+    textStyles.push({
+      color,
+    });
+  } else if (color) {
+    buttonStyles.push({
+      backgroundColor: color,
+    });
+  }
+  if (disabled) {
+    buttonStyles.push(styles.buttonDisabled);
+    textStyles.push(styles.textDisabled);
+  }
+  const formattedTitle = Platform.OS === 'android'
+    ? title.toUpperCase()
+    : title;
+  const accessibilityTraits = ['button'];
+  if (disabled) {
+    accessibilityTraits.push('disabled');
+  }
+
+  function TouchableWithProps({ children, onPress }) {
+    return (
+      <Touchable
+        accessibilityComponentType="button"
+        accessibilityLabel={accessibilityLabel}
+        accessibilityTraits={accessibilityTraits}
+        testID={testID}
+        disabled={disabled}
+        onPress={onPress}
+      >
+        {children}
+      </Touchable>
+    );
+  }
+
+  TouchableWithProps.propTypes = {
+    onPress: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired,
+  };
+
+  return (
+    <Link to={to} replace={replace} component={TouchableWithProps}>
+      <View style={buttonStyles}>
+        <Text style={textStyles}>{formattedTitle}</Text>
+      </View>
+    </Link>
+  );
+}
+
+RouterButton.propTypes = {
+  /**
+     * Text to display inside the button
+     */
+  title: PropTypes.string.isRequired,
+  /**
+     * Text to display for blindness accessibility features
+     */
+  accessibilityLabel: PropTypes.string,
+  /**
+     * Color of the text (iOS), or background color of the button (Android)
+     */
+  color: ColorPropType,
+  /**
+     * If true, disable all interactions for this component.
+     */
+  disabled: PropTypes.bool,
+  /**
+     * If true, replace the current point in history rather than push
+     */
+  replace: PropTypes.bool,
+  /**
+     * Handler to be called when the user taps the button
+     */
+  testID: PropTypes.string,
+  /**
+     * Used to determine which path to link to
+     */
+  to: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object
+  ]),
+};
+
+RouterButton.defaultProps = {
+  replace: false,
+  disabled: false,
+  testID: null,
+  color: null,
+  accessibilityLabel: null,
+};
+
+export default RouterButton;
